27aad34119989b8395ec2b6c1deda0345db18054
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-19 20:08:12 -0300

    Questão3

---
+++ lista1/Question3.java
@@ -0,0 +1,15 @@
+//3. Informar um saldo e imprimir o saldo com reajuste de 1%.
+package lista1;
+import java.util.Scanner;
+public class Question3 {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        double saldo;
+        double reajuste = 0.01; //1% de reajuste
+        System.out.println("Digite seu saldo:");
+        saldo = scanner.nextDouble();
+        double saldoReajustado = saldo + (saldo * reajuste);
+        System.out.println("Seu saldo com reajuste de 1% é: " + saldoReajustado);
+        scanner.close();
+    }
+}
