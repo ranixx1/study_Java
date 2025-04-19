056a25d02f049914225382a81989f1db6cec6dad
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-19 20:21:50 -0300

    Questão4

---
+++ lista1/Question4.java
@@ -0,0 +1,20 @@
+//Faça um programa que leia uma temperatura em Celsius e a converta para Fahrenheit.
+//Fórmula: F = C * 1.8 + 32
+package lista1;
+
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em Celsius: ");
+        float celsius = scanner.nextFloat();
+
+        double fahrenheit = celsius * 1.8 + 32;
+
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+
+        scanner.close();
+    }
+}
