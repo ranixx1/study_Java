634ae4dbfb5c3e0ad2d850cd550b786a8256bd55
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-22 23:39:34 -0300

    Question12 book

---
+++ lista1/Question12.java
@@ -0,0 +1,15 @@
+package lista1;
+//crie um programa que peça um número e dado o número subtraia 1 até zero
+import java.util.Scanner;
+public class Question12 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("digite um número:");
+        int i=scanner.nextInt();
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+        scanner.close();
+    }
+}
