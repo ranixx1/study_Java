4315bf08f6a0b4785c87d58e8219d7dfec45e3b3
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-22 23:57:27 -0300

    Question13 book [for]

---
+++ lista1/Question14.java
@@ -0,0 +1,16 @@
+package lista1;
+//crie com o operado do e while um que peça um numero ao usúario e ele conte do 0, 1,2 ate numeros vezes com for
+
+import java.util.Scanner;
+public class Question14 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("digite um número:");
+        int numero = scanner.nextInt();
+        for(int i = 0; i<numero; i++) {
+            System.out.println((i));
+        }
+        scanner.close();
+    }
+    
+}
