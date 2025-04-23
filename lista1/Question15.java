78136dd60834b73775f4269f8fb6bed95db076c9
ranilton <ranilton.l@escolar.ifrn.edu.br>
2025-04-23 00:13:35 -0300

    Question15 ex book [continue]

---
+++ lista1/Question15.java
@@ -0,0 +1,15 @@
+package lista1;
+// contar quantas vezes aparece o nome Bia na lista
+public class Question15 {
+    public static void main(String[]args){
+        String alunos[] = {"Ana","Bia","Lucas","Pedro","Lucas","Bia"};
+        int count=0;
+        for(int i = 0; i< alunos.length; i++){
+            if(!alunos[i].equals("Bia")){
+                continue; //vai para a proxima iteração
+            }
+            count++;
+        }
+        System.err.println("encontrado "+count+" vezes o nome de Bia no cadastro!!!");
+    }
+}
