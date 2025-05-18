44f68837e4decb7c157de7273ee4c18d144d25a9
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-18 18:54:08 -0300

    study loopings

---
+++ arrays/LoopingComArrays.java
@@ -0,0 +1,13 @@
+//pag 94
+package arrays;
+
+public class LoopingComArrays {
+    public static void main(String[]args){
+      int[]ages = new int[100];
+      for(int i=0; i<100;i++){
+        ages[i]=i;
+        System.out.println(ages[i]);
+      }
+
+    }
+}
