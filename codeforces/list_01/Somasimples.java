a1aa9df1495612986630954f857e699bc4872ec0
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-23 22:54:20 -0300

    go back study java, add soma simples intereger

---
+++ codeforces/list_01/Somasimples.java
@@ -0,0 +1,15 @@
+package codeforces.list_01;
+
+import java.util.Scanner;
+public class Somasimples {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" DIGITE DOIS NÚMEROS INTEIROS ");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        System.out.println("A soma é:" + (n1+n2));
+
+        scanner.close();
+    }
+
+}
