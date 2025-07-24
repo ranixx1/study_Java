5954d9e29672e54c4f4a5a226807c4fcf369cc4a
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-23 23:10:27 -0300

    feat: printf format double sum

---
+++ codeforces/list_01/Somafloat.java
@@ -0,0 +1,12 @@
+package codeforces.list_01;
+
+public class Somafloat {
+    public static void main(String[]args){
+        double n1 = 6.78;
+        double n2 = 8.90;
+        double sum = n1+n2;
+
+        System.out.printf("A soma é: %.3f%n", sum); // Usa printf para mudar o formato da saída
+
+    }
+}
