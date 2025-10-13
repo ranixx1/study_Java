124a527a44219f4f122c43a1341f674bc6789275
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-10-13 15:53:11 -0300

    feat: adiciona classe Password para validação de senha

---
+++ src/exercicios/beecrowd/lista3_Lucena/Password.java
@@ -0,0 +1,20 @@
+package exercicios.beecrowd.lista3_Lucena;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String senha = scanner.nextLine();
+        while (!senha.equals("2002")) {
+            System.out.println("Senha Invalida");
+            senha = scanner.nextLine();
+        }
+        System.out.println("Acesso Permitido");
+
+        scanner.close();
+
+    }
+
+}
