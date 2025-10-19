bf2abc7dbe3c89307a08ca07cedbe4d744a23cc9
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-10-19 19:30:48 -0300

    feat: implementa programa que conta de 0 a 10 e imprime os números

---
+++ src/exercicios/lista_braulio_test/q03.java
@@ -0,0 +1,10 @@
+//Faça um programa que implementa um laço com um teste no início que conte de 0 a 10 imprimindo esses números, um em cada linha da saída.
+package exercicios.lista_braulio_test;
+
+public class q03 {
+    public static void main(String[]args){
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
