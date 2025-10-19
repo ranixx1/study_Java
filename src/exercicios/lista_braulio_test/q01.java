73636a30d6405d324b7120aecec5bccc5ffca3cc
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-10-19 19:30:19 -0300

    feat: implementa programa para calcular e mostrar os 10 primeiros múltiplos de 3

---
+++ src/exercicios/lista_braulio_test/q01.java
@@ -0,0 +1,12 @@
+//Faça um programa que calcule e mostre os 10 primeiros números múltiplos de 3, considerando valores maiores que 0
+package exercicios.lista_braulio_test;
+
+public class q01 {
+    public static void main(String[]args){
+        int n=0;
+        for(int i=1;i<=10;i++){
+            n=n+3;
+            System.out.println(n);
+
+    }}
+}
